651f6390849a78d8f0df7280bef87816fcea20c0
Kirillgorbachev <150475173+Kirillgorbachev@users.noreply.github.com>
2024-07-11 12:08:23 +0300

    005: Поменяны размерности страницы регистрации на rem, поменяна страница регистрации по стилю так же, как страница логина, убрано подсвечивание красным и зеленым inputов, исправлена проверка на валидацию после нажатия кнопки (#13)

--- frontend-client/src/pages/register/Register.jsx
+++ frontend-client/src/pages/register/Register.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import styles from "./RegisterForm.module.scss";
-//import {useRegisterMutation} from "../authApi";
-import {useNavigate} from "react-router-dom";
-
+import styles from "./register.module.scss";
+// import { useRegisterMutation } from "../authApi";
+import { useNavigate } from "react-router-dom";
 
 const NAME_REGEX = /^([а-яёА-ЯЁ]){1,23}$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[0-9]).{8,24}$/;
 const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-// export const Register = () => {
-//     React.useLayoutEffect(() => {
-//         window.scrollTo(0, 0);
-//     });
 
 const Register = () => {
     const [firstName, setFirstName] = useState("");
@@ -33,108 +27,59 @@ const Register = () => {
     const [matchingPassword, setMatchingPassword] = useState("");
     const [validMatchingPassword, setValidMatchingPassword] = useState(true);
 
-    //const [register] = useRegisterMutation();
+    // const [register] = useRegisterMutation();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        setValidLastName(
-            NAME_REGEX.test(lastName) || lastName === "" ? true : false
-        );
-    }, [lastName]);
+    const validFields = () => {
+        const isValidFirstName = NAME_REGEX.test(firstName);
+        const isValidMiddleName = NAME_REGEX.test(middleName);
+        const isValidLastName = NAME_REGEX.test(lastName);
+        const isValidEmail = EMAIL_REGEX.test(email);
+        const isValidPassword = PASSWORD_REGEX.test(password);
+        const isValidMatchingPassword = password === matchingPassword;
 
-    useEffect(() => {
-        setValidFirstName(
-            NAME_REGEX.test(firstName) || firstName === "" ? true : false
-        );
-    }, [firstName]);
+        setValidFirstName(isValidFirstName);
+        setValidMiddleName(isValidMiddleName);
+        setValidLastName(isValidLastName);
+        setValidEmail(isValidEmail);
+        setValidPassword(isValidPassword);
+        setValidMatchingPassword(isValidMatchingPassword);
 
-    useEffect(() => {
-        setValidMiddleName(
-            NAME_REGEX.test(middleName) || middleName === "" ? true : false
-        );
-    }, [middleName]);
-
-    useEffect(() => {
-        setValidEmail(EMAIL_REGEX.test(email) || email === "" ? true : false);
-    }, [email]);
-
-    useEffect(() => {
-        setValidPassword(
-            PASSWORD_REGEX.test(password) || password === "" ? true : false
-        );
-    }, [password]);
-
-    useEffect(() => {
-        setValidMatchingPassword(
-            matchingPassword === "" || password === matchingPassword ? true : false
-        );
-    }, [password, matchingPassword]);
-
-    const validFields = () =>{
-        return validEmail
-            && validPassword
-            && validMatchingPassword
-            && validFirstName
-            && validMiddleName
-            &&validLastName;
-    }
+        return isValidFirstName && isValidMiddleName && isValidLastName && isValidEmail && isValidPassword && isValidMatchingPassword;
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (lastName === "") {
-            setValidLastName(false);
+        if (validFields()) {
+            // try {
+            //     const response = await register({
+            //         email: email,
+            //         password: password,
+            //         firstName: firstName,
+            //         middleName: middleName,
+            //         lastName: lastName
+            //     }).unwrap();
+            //     navigate('/login');
+            // } catch (error) {
+            //     console.log(error);
+            // }
         }
-        if (firstName === "") {
-            setValidFirstName(false);
-        }
-        if (middleName === "") {
-            setValidMiddleName(false);
-        }
-        if (email === "") {
-            setValidEmail(false);
-        }
-        if (password === "") {
-            setValidPassword(false);
-        }
-        if (matchingPassword === "") {
-            setValidMatchingPassword(false);
-        }
-
-        // if(validFields()){
-        //     try{
-        //         const response = await register({
-        //             email: email,
-        //             password: password,
-        //             firstName: firstName,
-        //             middleName: middleName,
-        //             lastName: lastName}).unwrap();
-        //         navigate('/login')
-        //     }
-        //     catch(error){
-        //         console.log(error);
-        //     }
-        // }
     };
 
     return (
         <>
-            <div className={styles.register__wrapper}>
-                <section className={styles.register__section}>
-                    <form className={styles.register__form} onSubmit={handleSubmit}>
-                        <h1 className={styles.register__header}>Регистрация</h1>
-                        <div className={styles.register__inputWrapper}>
-                            <label className={styles.register__label} htmlFor="nickname">
+            <div className={styles.wrapper}>
+                <section className={styles.section}>
+                    <form className={styles.form} onSubmit={handleSubmit}>
+                        <h1 className={styles.header}>Регистрация</h1>
+
+                        <div className={styles.inputWrapper}>
+                            <label className={styles.label} htmlFor="nickname">
                                 Никнэйм:
                             </label>
                             <input
-                                className={
-                                    validMiddleName
-                                        ? middleName !== ""
-                                            ? `${styles.register__input} ${styles.register__inputBoxShadowGreen}`
-                                            : styles.register__input
-                                        : `${styles.register__input} ${styles.register__inputBoxShadowRed}`
-                                }
+                                className={styles.input}
                                 type="text"
                                 id="nickname"
                                 placeholder="Никнэйм"
@@ -145,8 +90,8 @@ const Register = () => {
                             <div
                                 className={
                                     !validMiddleName
-                                        ? styles.register__errorMessage
-                                        : `${styles.register__errorMessage} ${styles.register__errorMessageHidden}`
+                                        ? styles.errorMessage
+                                        : `${styles.errorMessage} ${styles.errorMessageHidden}`
                                 }
                                 id="middle-name"
                             >
@@ -157,18 +102,12 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <div className={styles.register__inputWrapper}>
-                            <label className={styles.register__label} htmlFor="email">
+                        <div className={styles.inputWrapper}>
+                            <label className={styles.label} htmlFor="email">
                                 Электронная почта:
                             </label>
                             <input
-                                className={
-                                    validEmail
-                                        ? email !== ""
-                                            ? `${styles.register__input} ${styles.register__inputBoxShadowGreen}`
-                                            : styles.register__input
-                                        : `${styles.register__input} ${styles.register__inputBoxShadowRed}`
-                                }
+                                className={styles.input}
                                 type="text"
                                 id="email"
                                 placeholder="Электронная почта"
@@ -182,8 +121,8 @@ const Register = () => {
                             <div
                                 className={
                                     !validEmail
-                                        ? styles.register__errorMessage
-                                        : `${styles.register__errorMessage} ${styles.register__errorMessageHidden}`
+                                        ? styles.errorMessage
+                                        : `${styles.errorMessage} ${styles.errorMessageHidden}`
                                 }
                                 id="email"
                             >
@@ -191,18 +130,12 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <div className={styles.register__inputWrapper}>
-                            <label className={styles.register__label} htmlFor="password">
+                        <div className={styles.inputWrapper}>
+                            <label className={styles.label} htmlFor="password">
                                 Пароль:
                             </label>
                             <input
-                                className={
-                                    validPassword
-                                        ? password !== ""
-                                            ? `${styles.register__input} ${styles.register__inputBoxShadowGreen}`
-                                            : styles.register__input
-                                        : `${styles.register__input} ${styles.register__inputBoxShadowRed}`
-                                }
+                                className={styles.input}
                                 type="password"
                                 id="password"
                                 placeholder="Пароль"
@@ -212,8 +145,8 @@ const Register = () => {
                             <div
                                 className={
                                     !validPassword
-                                        ? styles.register__errorMessage
-                                        : `${styles.register__errorMessage} ${styles.register__errorMessageHidden}`
+                                        ? styles.errorMessage
+                                        : `${styles.errorMessage} ${styles.errorMessageHidden}`
                                 }
                                 id="password"
                             >
@@ -225,18 +158,12 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <div className={styles.register__inputWrapper}>
-                            <label className={styles.register__label} htmlFor="matching-password">
+                        <div className={styles.inputWrapper}>
+                            <label className={styles.label} htmlFor="matching-password">
                                 Повторите пароль:
                             </label>
                             <input
-                                className={
-                                    validMatchingPassword
-                                        ? matchingPassword !== ""
-                                            ? `${styles.register__input} ${styles.register__inputBoxShadowGreen}`
-                                            : styles.register__input
-                                        : `${styles.register__input} ${styles.register__inputBoxShadowRed}`
-                                }
+                                className={styles.input}
                                 type="password"
                                 id="matching-password"
                                 placeholder="Повторите пароль"
@@ -246,8 +173,8 @@ const Register = () => {
                             <div
                                 className={
                                     !validMatchingPassword
-                                        ? styles.register__errorMessage
-                                        : `${styles.register__errorMessage} ${styles.register__errorMessageHidden}`
+                                        ? styles.errorMessage
+                                        : `${styles.errorMessage} ${styles.errorMessageHidden}`
                                 }
                                 id="matching-password"
                             >
@@ -255,7 +182,7 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <button className={styles.register__button}>Создать аккаунт</button>
+                        <button className={styles.button}>Создать аккаунт</button>
                     </form>
                 </section>
             </div>
@@ -263,4 +190,4 @@ const Register = () => {
     );
 };
 
-export {Register};
+export { Register };
